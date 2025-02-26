13385b6d8cad868801d3b8fcebc6f678f7a5a17f
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2025-02-26 23:44:04 +0100

    soft baby responsive

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,12 +5,24 @@ import Link from "next/link";
 import Image from "next/image";
 import { Arvo } from "next/font/google";
 import { useTypewriter } from "react-simple-typewriter";
+import { motion } from 'motion/react'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const arvoFont= Arvo ({
   subsets: ["latin"],
   weight: '400',
   variable: "--font-arvo",
 });
+
 
 
 const Hero = () => {
@@ -24,7 +36,7 @@ const Hero = () => {
       {/* SECTION 1 */}
         <section id='section1' className='h-screen bg-cover  bg-center bg-no-repeat' style={{ backgroundImage: "url('/image/bg-sec1.avif')" }}>
             <div className='bg-white h-[60%] md:h-[60%] text-3xl md:px-[3rem] '>
-              <h1 className="text-base pt-[4rem] md:pt-[8rem] font-semibold text-center font-arvo">Welcome to</h1>
+              <h1 className="text-lg pt-[4rem] md:pt-[8rem] font-semibold text-center font-arvo">Welcome to</h1>
               <h1 className='flex items-center justify-center pt-[1rem] md:pt-[1rem] font-arvo head_text tracking-[5px] text-center'>{text}</h1>
               <p className='text-center text-base md:text-[20px] pt-[2rem] font-arvo'>We are a drinking club with a running problem.</p>
               <p className='text-center pt-[1rem] text-sm font-arvo md:text-xl'>We drink beer to save water</p>
@@ -81,7 +93,7 @@ const Hero = () => {
         </section>
 
         {/* SECTION 3 */}
-        <div className="px-[3rem] bg-gray-300/20 py-16 relative">
+        <div id="section3" className="px-[3rem] bg-gray-300/20 py-16 relative">
           <div>
             <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740592027/pic1_mss4ds.jpg' width={700} height={500} alt="image" className="rounded-xl"/>
           </div>
@@ -89,7 +101,7 @@ const Hero = () => {
           <div className="md:absolute top-[5rem] pr-[16px] pb-5 right-[8rem] font-arvo h-[35%] md:bg-red-100/80 w-fit rounded-lg">
             <div className="right-[24rem] h-[100%] bg-gray-50/90  px-[1rem] py-10 rounded-lg">
               <h1 className="font-bold text-2xl ">Hiking with your running shoes.</h1>
-              <p className="pt-3 leading-5">Hiking with your running shoes lacks proper ankle support, <br /> compromises stability and durability.  compared to proper hiking boots <br /> which provide better grip and stability. Confirm from DGM Bigger  <br /> Twerk for more information else OTILO!</p>
+              <p className="pt-3 leading-5">Hiking with your running shoes lacks proper ankle support, <br /> compromises stability and durability compared to proper hiking boots <br /> which provide better grip and stability. Confirm from DGM Bigger  <br /> Twerk for more information else OTILO!</p>
             </div>
 
             <div className="flex md:block px-[1rem]">
@@ -100,6 +112,169 @@ const Hero = () => {
 
           </div>
 
+
+        </div>
+
+        {/* Section 4 Swiper Js */}
+        <div id="section4" className="md:px-[3rem] px-[1rem] bg-gray-300/20 pb-[2rem]">
+        
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+            }}
+            navigation={true}
+            pagination={{
+            clickable: true,
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper">
+              
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599057/IMG-20250127-WA0090_byqiol.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599093/IMG-20250226-WA0181_bdgrka.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599103/IMG-20250226-WA0185_aztgr7.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599111/IMG-20250226-WA0183_abmju2.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599125/IMG-20250226-WA0187_ontlcb.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599130/IMG-20250226-WA0188_llnkam.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599144/IMG-20250226-WA0190_kl0scm.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599150/IMG-20250226-WA0191_uejhnr.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599182/IMG-20250226-WA0195_inzdiw.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599136/IMG-20250226-WA0189_q2ns0z.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599172/IMG-20250226-WA0193_aae8ow.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599248/IMG-20250226-WA0197_o9oz9f.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599260/IMG-20250226-WA0199_altyic.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599253/IMG-20250226-WA0198_zxstru.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className='h-48 w-full overflow-hidden'>
+                    <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740599268/IMG-20250226-WA0200_iiopxm.jpg' width={100} height={100}alt='image' className='rounded-[12px] h-full w-full object-cover'/>
+                  </div>
+              </SwiperSlide>
+
+
+
+          </Swiper>
+                
+
+        </div>
+          
+          
+        {/* Section 5 SOFT BABY */}
+        <div id="section5" className="bg-black text-gray-200/50 h-[40%] lg:flex font-arvo">
+          <div>
+            <Image src='https://res.cloudinary.com/dvkmv0k18/image/upload/v1740602752/DSC_5481_zen0c1.jpg' width={700} height={100} alt="image"/>
+          </div>
+          
+          <div className="flex flex-col items-center justify-center mx-auto pt-5">
+            <p className="leading-8 pb-[1.5rem] text-sm md:text-xl px-[1rem]">“Hashing is a life style and cold beer heals all wound. <br /> We hash to promote physical fitness among hashers, <br /> We hash to get rid of weekend hangovers, <br /> We hash to acquire a good thirst and to satisfy it with beer and <br /> To persuade the older hashers they are not as old as they feel.”
+            </p>
+
+            <div className="pt-5 flex gap-5">
+              <motion.div
+                initial={{
+                  opacity:0,
+              }}
+              animate={{
+                  scale:[1,2,2,3,1],
+                  opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+                  borderRadius: ['20%', '20%', '50%', '80%', '20%'],
+              }}
+      
+              transition={{
+                  duration:2.5,
+              }}
+              className='relative flex justify-center items-center mt'>
+                <div className='absolute border border-blue-400/50 rounded-full h-[150px] w-[150px] mt-4 animate-ping'/>
+
+                <div className='absolute border border-blue-200/50 rounded-full h-[250px] w-[250px] mt-4'/>
+
+                <div className='absolute border border-[#F7AB0A]/50 rounded-full h-[350px] w-[350px] mt-4 animate-ping'/>
+
+                {/* <div className='rounded-full border border-[#F7AB0A]/50 opacity-20 h-[500px] w-[500px] absolute mt-52 animate-pulse'/> */}
+                <Image src='/image/softb.jpg' width={100} height={10} alt="profile image" className="ring ring-blue-400 rounded-full" />
+              </motion.div>
+              
+              
+              <ul className="flex flex-col items-center justify-center text-white">
+                <li className="font-semibold">SOFT BABY</li>
+                <li className="text-sm">PH H<sup>3</sup></li>
+              </ul>
+            </div>
+
+          </div>
 
         </div>
       
